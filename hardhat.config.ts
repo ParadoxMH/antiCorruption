34c7e729b781dc28e35a80f09c5004acbccf64a2
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-03-09 22:42:23 +0200

    add polygon networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,15 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "";
+const DEV_ACCOUNT_PRIVATE_KEY = process.env.DEV_ACCOUNT_PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  //npx hardhat run scripts/deploy.ts --network goerli
   networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+    //npx hardhat run scripts/deploy.ts --network polygon
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [DEV_ACCOUNT_PRIVATE_KEY]
+    },
+    //npx hardhat run scripts/deploy.ts --network mumbai
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [DEV_ACCOUNT_PRIVATE_KEY]
     }
   }
 };
