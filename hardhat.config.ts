b25887be4e538397174cc2d147a63f0dcfbdc3e5
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-03-11 09:10:58 +0200

    add use of solidity-coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
+import 'solidity-coverage'
 
 const DEV_ACCOUNT_PRIVATE_KEY = process.env.DEV_ACCOUNT_PRIVATE_KEY || "";
 
