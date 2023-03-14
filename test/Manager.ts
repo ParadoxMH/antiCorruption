78bbcfef5362ef28a6ae74b400f15ad0a982cc89
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-03-14 18:46:14 +0200

    add unit tests

--- test/Manager.ts
+++ test/Manager.ts
@@ -49,8 +49,99 @@ describe("Manager", function () {
     });
 
   });
+  
+  describe("Interfaces", function () {
+    it("Manager should support IERC165", async function () {
+      const { manager } = await loadFixture(deployManager);
+      const supportsIERC165 = await manager.supportsInterface("0x01ffc9a7");
 
-  describe("Roles", function () {
+      expect(supportsIERC165).to.equal(true);
+    });
+    it("Manager should support IAccessControl", async function () {
+      const { manager } = await loadFixture(deployManager);
+      const supportsIAccessControl = await manager.supportsInterface("0x7965db0b");
+
+      expect(supportsIAccessControl).to.equal(true);
+    });
+  });
+
+  describe("Grant Roles", function () {
+
+    it("Only Admin can add Manager", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+
+        await manager.addManager(otherAccount.address);
+
+        expect(await manager.hasRole(manager.MANAGER_ROLE(), otherAccount.address)).to.equal(true);
+    });
+    it("Only Admin can remove Manager", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+
+        await manager.addManager(otherAccount.address);
+        expect(await manager.hasRole(manager.MANAGER_ROLE(), otherAccount.address)).to.equal(true);
+
+        await manager.removeManager(otherAccount.address);
+        expect(await manager.hasRole(manager.MANAGER_ROLE(), otherAccount.address)).to.equal(false);
+    });
     
+    it("Only Manager can add Donator", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+  
+        await manager.addManager(otherAccount.address);
+        await manager.connect(otherAccount).addDonator(otherAccount.address);
+  
+        expect(await manager.hasRole(manager.DONATOR_ROLE(), otherAccount.address)).to.equal(true);
+    });
+    it("Only Manager can remove Donator", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+
+        await manager.addManager(otherAccount.address);
+        await manager.connect(otherAccount).addDonator(otherAccount.address);
+  
+        expect(await manager.hasRole(manager.DONATOR_ROLE(), otherAccount.address)).to.equal(true);
+
+        await manager.connect(otherAccount).removeDonator(otherAccount.address);
+        expect(await manager.hasRole(manager.DONATOR_ROLE(), otherAccount.address)).to.equal(false);
+    });
+      
+    it("Only Manager can add Vendor", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+  
+        await manager.addManager(otherAccount.address);
+        await manager.connect(otherAccount).addVendor(otherAccount.address);
+  
+        expect(await manager.hasRole(manager.VENDOR_ROLE(), otherAccount.address)).to.equal(true);
+    });
+    it("Only Manager can remove Vendor", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+
+        await manager.addManager(otherAccount.address);
+        await manager.connect(otherAccount).addVendor(otherAccount.address);
+  
+        expect(await manager.hasRole(manager.VENDOR_ROLE(), otherAccount.address)).to.equal(true);
+
+        await manager.connect(otherAccount).removeVendor(otherAccount.address);
+        expect(await manager.hasRole(manager.VENDOR_ROLE(), otherAccount.address)).to.equal(false);
+    });
+
+    it("Only Manager can add Viewer", async function () {
+      const { manager, otherAccount } = await loadFixture(deployManager);
+
+      await manager.addManager(otherAccount.address);
+      await manager.connect(otherAccount).addViewer(otherAccount.address);
+
+      expect(await manager.hasRole(manager.VIEWER_ROLE(), otherAccount.address)).to.equal(true);
+    });
+    it("Only Manager can remove Viewer", async function () {
+        const { manager, otherAccount } = await loadFixture(deployManager);
+
+        await manager.addManager(otherAccount.address);
+        await manager.connect(otherAccount).addViewer(otherAccount.address);
+  
+        expect(await manager.hasRole(manager.VIEWER_ROLE(), otherAccount.address)).to.equal(true);
+
+        await manager.connect(otherAccount).removeViewer(otherAccount.address);
+        expect(await manager.hasRole(manager.VIEWER_ROLE(), otherAccount.address)).to.equal(false);
+    });
   });
 });
