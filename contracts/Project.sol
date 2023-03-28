550a7249fe3611c35d63d7ee6865a9a18515c6be
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-03-28 20:51:02 +0300

    update Project to work with Manager

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,28 +1,60 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Project {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Manager.sol";
+
+contract Project is Ownable {
 
     mapping(address => bool) whitelistedVendors;
     mapping(address => bool) whitelistedViewers;
+    Manager manager;
 
-    function whitelistVendor(address _vendor) public onlyRole(MANAGER_ROLE) {
-        require(hasRole(VENDOR_ROLE, _vendor), 'Only Vendor address can be whitelisted.');
-        whitelistedDonators[_vendor] = true;
+    constructor(address _manager) {
+        manager = Manager(_manager);
     }
 
-    function whitelistVendors(address[] calldata _vendors) external onlyRole(MANAGER_ROLE) {
+    function setManagerAddress(address _addr) public onlyOwner {
+        manager = Manager(_addr);
+    }
+
+    modifier onlyRole(bytes32 role) {
+        _checkRole(role, msg.sender);
+        _;
+    }
+
+    function _checkRole(bytes32 role, address account) internal view virtual {
+        if (!manager.hasRole(role, account)) {
+            revert(
+                string(
+                    abi.encodePacked(
+                        "AccessControl: account ",
+                        Strings.toHexString(account),
+                        " is missing role ",
+                        Strings.toHexString(uint256(role), 32)
+                    )
+                )
+            );
+        }
+    }
+
+    function whitelistVendor(address _vendor) public onlyRole(manager.MANAGER_ROLE()) {
+        require(manager.hasRole(manager.VENDOR_ROLE(), _vendor), 'Only Vendor address can be whitelisted.');
+        whitelistedVendors[_vendor] = true;
+    }
+
+    function whitelistVendors(address[] calldata _vendors) external onlyRole(manager.MANAGER_ROLE()) {
         for (uint i = 0; i < _vendors.length; i++) {
             whitelistVendor(_vendors[i]);
         }
     }
 
-    function whitelistViewer(address _viewer) public onlyRole(MANAGER_ROLE) {
-        require(hasRole(VIEWER_ROLE, _viewer), 'Only Viewer address can be whitelisted.');
-        whitelistedDonators[_viewer] = true;
+    function whitelistViewer(address _viewer) public onlyRole(manager.MANAGER_ROLE()) {
+        require(manager.hasRole(manager.VIEWER_ROLE(), _viewer), 'Only Viewer address can be whitelisted.');
+        whitelistedViewers[_viewer] = true;
     }
 
-    function whitelistViewers(address[] calldata _viewers) external onlyRole(MANAGER_ROLE) {
+    function whitelistViewers(address[] calldata _viewers) external onlyRole(manager.MANAGER_ROLE()) {
         for (uint i = 0; i < _viewers.length; i++) {
             whitelistViewer(_viewers[i]);
         }
