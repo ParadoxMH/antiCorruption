148adba2a2d2b3307e7bb082d3d7f8c2287790f2
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-03-28 16:05:10 +0300

    update Register contract

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -1,16 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Register {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Manager.sol";
 
-    mapping(address => bool) whitelistedDonators;
+contract Register is Ownable {
 
-    function whitelistDonator(address _donator) public onlyRole(MANAGER_ROLE) {
-        require(hasRole(DONATOR_ROLE, _donator), 'Only Donator address can be whitelisted.');
+    mapping(address => bool) public whitelistedDonators;
+    Manager manager;
+
+    constructor(address _manager) {
+        manager = Manager(_manager);
+    }
+
+    function setManagerAddress(address _addr) public onlyOwner {
+        manager = Manager(_addr);
+    }
+
+    modifier onlyRole(bytes32 role) {
+        _checkRole(role, msg.sender);
+        _;
+    }
+
+    function _checkRole(bytes32 role, address account) internal view virtual {
+        if (!manager.hasRole(role, account)) {
+            revert(
+                string(
+                    abi.encodePacked(
+                        "AccessControl: account ",
+                        Strings.toHexString(account),
+                        " is missing role ",
+                        Strings.toHexString(uint256(role), 32)
+                    )
+                )
+            );
+        }
+    }
+
+    function whitelistDonator(address _donator) public onlyRole(manager.MANAGER_ROLE()) {
+        require(manager.hasRole(manager.DONATOR_ROLE(), _donator), 'Only Donator address can be whitelisted.');
         whitelistedDonators[_donator] = true;
     }
 
-    function whitelistDonators(address[] calldata _donators) external onlyRole(MANAGER_ROLE) {
+    function whitelistDonators(address[] calldata _donators) external onlyRole(manager.MANAGER_ROLE()) {
         for (uint i = 0; i < _donators.length; i++) {
             whitelistDonator(_donators[i]);
         }
