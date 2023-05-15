31429a399b10604b482cb2e08f5f1c2e11e65220
Manolii Horditsa <mhorditsa@oncologyanalytics.com>
2023-05-15 17:04:16 +0300

    update deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,15 @@ import 'solidity-coverage'
 const DEV_ACCOUNT_PRIVATE_KEY = process.env.DEV_ACCOUNT_PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 4000,
+      },
+    },
+  },
   networks: {
     hardhat: {
       gas: 12000000,
@@ -21,7 +29,10 @@ const config: HardhatUserConfig = {
     //npx hardhat run scripts/deploy.ts --network mumbai
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [DEV_ACCOUNT_PRIVATE_KEY]
+      accounts: [DEV_ACCOUNT_PRIVATE_KEY],
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true
     }
   }
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,34 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  var managerStorage = await deployManagerStorage();
+  var registerFactory = await deployRegisterFactory();
+
+  await deployManager(managerStorage.address, registerFactory.address);
+}
+async function deployManagerStorage() { 
+  const ManagerStorage = await ethers.getContractFactory("ManagerStorage");
+  const ctract = await ManagerStorage.deploy();
+
+  console.log(`ManagerStorage deployed to ${ctract.address}`);
+
+  return ctract;
+}
+async function deployRegisterFactory() {
+  const RegisterFactory = await ethers.getContractFactory("RegisterFactory");
+  const ctract = await RegisterFactory.deploy();
+
+  console.log(`RegisterFactory deployed to ${ctract.address}`);
+
+  return ctract;
+}
+async function deployManager(managerStorage: string, registerFactory: string) { 
   const Manager = await ethers.getContractFactory("Manager");
-  const manager = await Manager.deploy();
+  const manager = await Manager.deploy(managerStorage, registerFactory);
 
-  await manager.deployed();
+  console.log(`Manager deployed to ${manager.address}`);
 
-  console.log(
-    `Manager deployed to ${manager.address}`
-  );
+  return manager;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
